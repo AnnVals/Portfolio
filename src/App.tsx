9856934bc48766fa414529fb394cae5d080eab88
AnnVals <anceninpil@gmail.com>
2026-02-16 13:21:27 +0100

    fix import statement for i18n configuration in App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Timeline from './components/Timeline';
 import About from './components/About';
 import TechnicalSkills from './components/Technicalskills';
 import ContactMe from './components/Contactme';
-import i18n from './i18n/config';
+import './i18n/config';
 import LanguageSelector from './components/LanguageSelector';
 import './App.css'
 
